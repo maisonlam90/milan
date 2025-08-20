94e00e2138095cfc0b0e357f27ed5e48bbd1687e
Mailan <maisonlam90@gmail.com>
2025-08-20 17:45:28 +0700

    doi ten module acl thanh ial, them group role

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -20,3 +20,5 @@ cargo sqlx migrate run
 //xoa schema csdl
 psql "postgres://admin:Maisonlan123@ap-southeast-1.e4c6174f-6538-4e47-93bf-0a2503819047.aws.yugabyte.cloud:5433/yugabyte?ssl=true&sslmode=require" \
   -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+
+dfgdfg
